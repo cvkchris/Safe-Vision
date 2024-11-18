dd42cf113f0204c1d9d5dd60be8a3586138375e6
Chris Vinod Kurian <112372258+cvkchris@users.noreply.github.com>
2024-11-18 11:42:43 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,10 +1,10 @@
 import requests
 
 def send_telegram_image(image_file):
-    bot_token = '7465229273:AAGAQllb6Z9pu4KjId3WGhsCTE3ywlVjLlM'
+    bot_token = '<your telegram bot token>'
     url = f'https://api.telegram.org/bot{bot_token}/sendPhoto'
     data = {
-        'chat_id': '1398254880'
+        'chat_id': '<your telegram chat-id>'
     }
     files = {
         'photo': image_file
@@ -13,10 +13,10 @@ def send_telegram_image(image_file):
     return response.json()
 
 def send_telegram_message():
-    bot_token = '7465229273:AAGAQllb6Z9pu4KjId3WGhsCTE3ywlVjLlM'
+    bot_token = '<your telegram bot token>'
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     data = {
-        'chat_id': '1398254880',
+        'chat_id': '<your telegram chat-id>',
         'text': 'violence-detected'
     }
     response = requests.post(url, data=data)
